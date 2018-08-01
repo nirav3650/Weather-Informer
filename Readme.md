9022c799d582511bea712c6c5471cd87fe68c796
Nirav <nirav365@gmail.com>
2018-08-01 13:13:19 +0530

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+##The Weather Tool 
+
+A little weather tool that shows you the current weather in a LOCATION of
+    your choice. Provide the city name and optionally a two-digit country code.
+    Here are two examples:
+    1. London,UK
+    2. Canmore
+    You need a valid API key from OpenWeatherMap for the tool to work. You can
+    sign up for a free account at https://openweathermap.org/appid.
+    
+
+The tool shows type of weather, temperature and humidty of the location provided.
---
+++ weatherinfo.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  1 12:10:15 2018
+
+@author: NIRAV.KAKKAD
+"""
+
+import requests
+import json
+
+
+class WeatherInfo:
+    __weather_key = 'b1b15e88fa797225412429c1c50c122a1'
+
+    def __init__(self):
+        print("Welcome to Weather Information")
+
+    def current_weather(self, location):
+        url = 'https://api.openweathermap.org/data/2.5/weather'
+
+        query_params = {
+            'q': location,
+            'appid': self.__weather_key,
+        }
+
+        response = requests.get(url, params=query_params)
+        b = json.loads(response.content)
+        weather_description = b['weather'][0]['description']
+        weather_details = b['main']
+        wtemperature = float(weather_details['temp']) - 272.15
+        whumidity = weather_details['humidity']
+        return weather_description, wtemperature, whumidity
+
+
+if __name__ == "__main__":
+    location = "London,UK"
+    obj = WeatherInfo()
+    weather = obj.current_weather(location)
+    print(f"The weather details for {location} are:\nType: {weather[0]}\nTemperature: %.2f Celsius\nHumidity: {weather[2]} " % (
+        weather[1]))
+
